a06f3ed26f885d02932996684ef5939cc3c2a568
Ang Chen <chenang@outlook.com>
2025-05-13 17:26:12 +0800

    Update FFT array triangular script to enhance visualization by adjusting color limits and plot range for improved clarity.

--- examples/matlab/fft/fft_array_triangular.m
+++ examples/matlab/fft/fft_array_triangular.m
@@ -53,7 +53,7 @@ end
 
 % Display the original pattern
 subplot(1,2,1);
-imagesc(xaxis,yaxis,pattern); axis('image');
+imagesc(xaxis,yaxis,pattern); axis('image'); colormap(flipud(gray));
 title('Original Pattern (Triangular Lattice)');
 xlabel('X size (m)'); ylabel('Y size (m)');
 colorbar('EastOutside'); 
@@ -66,11 +66,11 @@ fft_magnitude = abs(fft_pattern);
 subplot(1,2,2);
 imagesc(fxaxis,fyaxis,fft_magnitude); axis('image');
 % Define plot range for Fourier transform (in cycles/meter)
-plot_range = 5e7; % 5e7 cycles/meter, adjust this value as needed
+plot_range = 1e8; % 5e7 cycles/meter, adjust this value as needed
 % Set the display range for x and y axes
 xlim([-plot_range plot_range]);
 ylim([-plot_range plot_range]);
-clim([0, 0.7e5]);  
+clim([0, 5e4]);  
 title('Fourier Transform');
 xlabel('Spatial Frequency (cycles/m)'); ylabel('Spatial Frequency (cycles/m)');
 colorbar('EastOutside'); 
